00d37fd0d1f3fb58780191cf3096c1d6ae903153
Don Park <don@donpark.org>
2012-11-21 13:13:57 -0800

    js cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,4 @@
 function ui_setup(prefix) {
-  $('li#menu-calendar').click(go_calendar)
-  $('li#menu-table').click(go_table)
-  go_calendar();
   $('#calendar-view').fullCalendar({
     editable: true,
     events: prefix+"/data",
@@ -10,29 +7,27 @@ function ui_setup(prefix) {
   })
 }
 
-function go_calendar() {
-  $('#menu-calendar').addClass('active')
-  $('#menu-table').removeClass('active')
-}
-
-function go_table(date) {
+function modal_add_transaction(date) {
   $('#menu-table').addClass('active')
   $('#menu-calendar').removeClass('active')
   var now = new XDate(date);
-  $('div#table input#date').attr('value', now.toString("yyyy-MM-dd"));
-  $('div#table').modal();
+  $('div#add-transaction input#date').attr('value', now.toString("yyyy-MM-dd"));
+  $('div#add-transaction').modal();
+}
+
+function modal_delete(record) {
+  $('form#detail').attr('action', e.valueId)
+  $('div#detail div#name').text(e.title)
+  $('div#detail div#date').text(e.start)
+  $('div#detail').modal();
 }
 
 function event_click(e) {
   if (e.valueId) {
-    console.log(e)
-    $('form#detail').attr('action', e.valueId)
-    $('div#detail div#name').text(e.title)
-    $('div#detail div#date').text(e.start)
-    $('div#detail').modal();
+    modal_delete(e)
   }
 }
 
 function day_click(e) {
-  go_table(e)
+  modal_add_transaction(e)
 }
